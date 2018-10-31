fb670f62d0ed209685d44ebb2124bcb17eb110e0
= <Jia.Liu@uoit.net>
2018-10-31 10:08:29 -0400

    merge attempt 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("IM IN BETA BRANCH NOW!");
 		System.out.println("Hello World!");	
+
+
+		System.out.println("IM IN BETA BRANCH NOW!(on a diff line)");
 	}
 }
